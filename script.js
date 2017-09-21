420a0c03f60746bde88f55bb44279aa30029a909
Ryan Waite <=>
2017-09-20 23:16:04 -0400

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var App = angular.module("myApp", []);
+
+App.controller("masterCtrl", ["$scope", function($scope){
+  $(document).ready(function(){
+
+    $.getJSON("http://quotes.rest/qod.json", (resp) => {
+      console.log(resp);
+      $scope.contents = resp
+      $scope.quote = resp.contents.quotes[0]
+      $scope.$apply(function(){console.log($scope);});
+    })
+    .error(() => {
+      alert("Error: No Data Came...");
+    });
+
+  });
+}])
